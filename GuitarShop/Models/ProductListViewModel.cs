1d379eda1515687f8f042bdcfca0f7e8c95e76ad
Worod Zik <worodzik@yahoo.com>
2022-03-12 01:03:12 -0500

    Add project files.

---
+++ GuitarShop/Models/ProductListViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GuitarShop.Models
+{
+    public class ProductListViewModel
+    {
+        public List<Category> Categories { get; set; }
+        public List<Product> Products { get; set; }
+        public String SelectedCategory { get; set; }
+        public String CheckActiveCategory(String category) =>
+            category == SelectedCategory ? "Active" : "";
+
+    }
+}
